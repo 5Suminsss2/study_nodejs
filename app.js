41bfee9d3de184ce090571459f126fcdb9fdefc1
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-02-22 15:47:59 +0900

    Section 18: 352 익스프레스 처리 요청 및 응답으로 서버 만들기

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-const http = require('http');
+const express = require('express');
 
+const app = express();
 
-// 서버 실행하면 실행되는 함수
-function handleRequest(req, res) {
-    if ( req.url === '/currenttime') {
-      res.statusCode = 200; // 요청이 성공했는지 여부를 알리는 방법 ( 응답 시의 HTTP 상태 코드를 지정한다)
-      res.end("<h1>"+ new Date().toISOString() + "</h1>"); // end를 실행해야 html 코드를 보낼 수 있음
-    } else if (req.url === '/') {
-      res.statusCode = 200;
-      res.end("<h1>Hello~~</h1>"); // end를 실행해야 html 코드를 보낼 수 있음
-    }
-    
-}
-const server = http.createServer(handleRequest);
+app.get('/currenttime', function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>"); // res.end와 같은 기능
+});
 
-server.listen(4000);
+app.get("/", function (req, res) {
+    res.end("<h1>Hello~~</h1>"); 
+});
+
+app.listen(4000);
