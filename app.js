8a4833a3d886c0ce25fd7b547d0535fa69df7ec3
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-02-23 18:03:43 +0900

    364. 양식 데이터 구문 분석 및 요청 리다이렉트

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require("path");
 
 const express = require('express');
@@ -5,6 +6,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public')); // 제공하려는 정적 파일을 보관해야하는 프로젝트의 폴더 이름
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
     const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -21,6 +23,20 @@ app.get("/recommend", function (req, res) {
   res.sendFile(htmlFilePath);
 });
 
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, 'data', 'restaurant.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm'); // confirm 페이지로 이동하기
+});
+
 app.get("/confirm", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "confirm.html");
   res.sendFile(htmlFilePath);
@@ -30,5 +46,6 @@ app.get("/about", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "about.html");
   res.sendFile(htmlFilePath);
 });
+
 
 app.listen(4000);
